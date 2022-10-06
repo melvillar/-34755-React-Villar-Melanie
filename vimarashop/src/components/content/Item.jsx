044cf87950b0791e587066ff85a8261f8a9b3418
melvillar <villar.melanie.araceli@gmail.com>
2022-10-05 22:52:18 -0300

    mostrando item

--- vimarashop/src/components/content/Item.jsx
+++ vimarashop/src/components/content/Item.jsx
@@ -1,22 +1,52 @@
 import React from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import "../styles/Products.css"
 
-const Item = ({producto}) => {
+
+const Item = ({}) => {
+    const [producto, setProducto] = useState()
+    const params = useParams();
+    const productId = params.id;
+    console.log (productId)
+
+
+const consultarBDD = async (ruta) => {
+    const response = await fetch(ruta)
+    const productos = await response.json()
+    console.log(productos)
+    return productos
+}
+
+useEffect(() => {
+    consultarBDD('../json/products.json').then(productos => {
+        const prod = productos.find (item =>item.id === productId)
+        console.log(prod)
+        setProducto(prod)
+    })
+
+}, []);
+
     return (
         <>
-            <div className="row g-0">
-    <div className="col-md-4">
-        <img src={`../assets/${producto.imagen}`} className="img-fluid rounded-start" alt="..." />
-    </div>
-    <div className="col-md-8">
-        <div className="card-body">
-            <h5 className="card-title">{producto.nombre}</h5>
-            <p className="card-text">{producto.precio}</p>
-            <p className="card-text">{producto.stock}</p>
-            <button className='btn btn-dark'>Agregar</button>
-        </div>
-        </div>
-    </div>
+            {
+                producto ? (
+                    <div className="row g-0">
+                        <div className="col-md-4">
+                            <img src={`../assets/${producto.imagen}`}  className="img-fluid rounded-start imgDetalle" alt="..." />
+                        </div>
+                        <div className="col-md-8">
+                            <div className="card-body detalle">
+                                    <h5 className="card-title">{producto.nombre}</h5>
+                                    <p className="card-text">{producto.precio}</p>
+                                    <p className="card-text">{producto.stock}</p>
+                                    <button className='btn btn-dark'>Agregar</button>
+                            </div>
+                        </div>
+                    </div>  
 
+                ): <h1>Loading...</h1>
+            }
         </>
     );
 }
